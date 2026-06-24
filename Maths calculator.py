b1b88ca976d3c9e4f8a364561637e529c60a1dd0
Sudhiksha-28 <Sudhihems@gmail.com>
2026-06-24 09:08:53 +0530

    Create Maths calculator.py

---
+++ Maths calculator.py
@@ -0,0 +1,13 @@
+# This is a comment. Python ignores this; it's just for you to read!
+
+# 1. Printing a simple text message
+print("Hello, World!")
+print("Welcome to Python programming! 🚀")
+
+# 2. Printing math numbers and calculations
+print("Let's do some quick math:")
+print(5 + 5)
+a=10
+# 3. Combining text and numbers together
+age = 19
+print("I am", age, "years old.")
